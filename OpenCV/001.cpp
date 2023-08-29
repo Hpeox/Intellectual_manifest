274d53801d48052db765038edeefc34b200c6d4f
H.peox <hpx.peipei@gmail.com>
2023-08-29 14:06:36 +0800

    Update 001.cpp

--- OpenCV/001.cpp
+++ OpenCV/001.cpp
@@ -7,13 +7,14 @@ using namespace std;
 int main()
 {
     cv::Mat image;
-    image = cv::imread("./data/2020/0/1.bmp");
+    image = cv::imread("D:/hpx/intellectual_manifest/CNN/Intellectual_manifest/OpenCV/data/2020/0/1.bmp");
     if (image.empty())
+    {
         cout<<"aaa"<<endl;
+        return 0;
+    }
     cv::namedWindow("image 1",cv::WINDOW_NORMAL);
     cv::imshow("image 1", image);
     cv::waitKey(0);
-    cv::destroyAllWindows();
-    cout<<"10"<<endl;
     return 0;
 }
