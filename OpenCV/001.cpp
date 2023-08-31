a4df894d5174e0e765f0b23bbe3a502a77bb914e
H.peox <hpx.peipei@gmail.com>
2023-08-31 19:55:56 +0800

    Update 001.cpp

--- OpenCV/001.cpp
+++ OpenCV/001.cpp
@@ -20,7 +20,7 @@ int calc_distance(int x,int y,int center_x,int center_y)
 int main()
 {
     cv::Mat image, gray, image_2;
-    image = cv::imread(IMAGE_1);
+    image = cv::imread(IMAGE_3);
     if (image.empty())
     {
         cout << "aaa" << endl;
@@ -94,11 +94,11 @@ int main()
             }
         }
     }
-    // test--
-    cout<<sqrt(dis_max)<<endl<<sqrt(dis_min);
-    cv::circle(contours,cv::Point(CENTER_BIAS,CENTER_BIAS),sqrt(dis_max),255);
-    cv::circle(contours,cv::Point(CENTER_BIAS,CENTER_BIAS),sqrt(dis_min),255);
-    // --test
+    // // test--
+    // cout<<sqrt(dis_max)<<endl<<sqrt(dis_min);
+    // cv::circle(contours,cv::Point(CENTER_BIAS,CENTER_BIAS),sqrt(dis_max),255);
+    // cv::circle(contours,cv::Point(CENTER_BIAS,CENTER_BIAS),sqrt(dis_min),255);
+    // // --test
     cv::namedWindow("contours", 1);
     cv::imshow("contours", contours);
 
